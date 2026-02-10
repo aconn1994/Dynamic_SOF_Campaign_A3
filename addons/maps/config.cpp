12f284383f6d1ac76f24af894eeb65f1c3364178
Adam Connolly <adam.m.connolly94@gmail.com>
2026-02-10 18:04:00 -0500

    update maps addon to handle multiplayer missions

--- addons/maps/config.cpp
+++ addons/maps/config.cpp
@@ -7,9 +7,19 @@ class CfgPatches {
         weapons[] = {};
         requiredVersion = 2.02;
         requiredAddons[] = {
+            "DSC_core",
             "DSC_main"
         };
         author = AUTHOR;
         authorUrl = "";
+    };
+};
+
+class CfgMissions {
+    class MPMissions {
+        class DSC_Altis {
+            briefingName = "Dynamic SOF Campaign - Altis";
+            directory = "z\DSC\addons\maps\DSC_Altis.Altis";
+        };
     };
 };
